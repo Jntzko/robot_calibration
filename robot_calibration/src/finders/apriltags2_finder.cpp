3f050876420b5d641444b57ab57047748bcaf301
Yannick Jonetzko <jonetzko@informatik.uni-hamburg.de>
2018-08-25 16:49:24 +0800

    AprilTags2 finder plugin
    The plugin detects apriltags and adds observation points for the origin and the conrners of an apriltag.
    
    These are the parameter that can be configured for one apriltag, currently the id and the size is indicated twice.
    
    apriltags2_finder:
        type: robot_calibration/AprilTags2Finder
        topic: /usb_cam/image_raw
        camera_info_topic: /usb_cam/camera_info
        camera_sensor_name: usb_cam
        chain_sensor_name: left_arm
        id: 0
        size: 0.046
        standalone_tags:
          [
          {id: 0, size: 0.046}
          ]
        tag_family:        'tag36h11' # options: tag36h11, tag36h10, tag25h9, tag25h7, tag16h5
        tag_border:        1          # default: 1
        tag_threads:       2          # default: 2
        tag_decimate:      1.0        # default: 1.0
        tag_blur:          0.0        # default: 0.0
        tag_refine_edges:  1          # default: 1
        tag_refine_decode: 0          # default: 0
        tag_refine_pose:   0          # default: 0
        tag_debug:         0          # default: 0
        publish_tf:        true       # default: false

---
+++ robot_calibration/src/finders/apriltags2_finder.cpp
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2015 Fetch Robotics Inc.
+ * Copyright (C) 2013-2014 Unbounded Robotics Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: Yannick Jonetzko
+
+#include <robot_calibration/capture/apriltags2_finder.h>
+#include <pluginlib/class_list_macros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <string>
+#include <tf/tf.h>
+
+
+PLUGINLIB_EXPORT_CLASS(robot_calibration::AprilTags2Finder, robot_calibration::FeatureFinder)
+
+namespace robot_calibration
+{
+
+AprilTags2Finder::AprilTags2Finder() :
+  waiting_(false)
+{
+}
+
+bool AprilTags2Finder::init(const std::string& name, ros::NodeHandle& nh)
+{
+  if (!FeatureFinder::init(name, nh))
+    return false;
+
+  tag_detector_ = new apriltags2_ros::TagDetector(nh);
+  // Setup Subscriber
+  std::string topic_name;
+  nh.param<std::string>("topic", topic_name, "/camera/image_raw");
+  subscriber_ = nh.subscribe(topic_name,
+                             1,
+                             &AprilTags2Finder::cameraCallback,
+                             this);
+
+  // Apriltag config
+  nh.param<int>("id", tag_id_, -1);
+  nh.param<double>("size", tag_size_, 1);
+  ROS_ERROR_STREAM(tag_id_ << " +++ " << tag_size_);
+
+
+  // Should we include debug image/cloud in observations
+  nh.param<bool>("debug", output_debug_, false);
+
+  // Name of the sensor model that will be used during optimization
+  nh.param<std::string>("camera_sensor_name", camera_sensor_name_, "camera");
+  nh.param<std::string>("chain_sensor_name", chain_sensor_name_, "arm");
+
+  // Publish where checkerboard points were seen
+  publisher_ = nh.advertise<sensor_msgs::PointCloud2>(getName() + "_points", 10);
+
+  // Setup to get camera info
+  if (!depth_camera_manager_.init(nh))
+  {
+    // Error will have been printed by manager
+    return false;
+  }
+
+  return true;
+}
+
+void AprilTags2Finder::cameraCallback(const sensor_msgs::Image::ConstPtr& image)
+{
+  if (waiting_)
+  {
+    image_ = *image;
+    waiting_ = false;
+  }
+
+}
+
+// Returns true if we got a message, false if we timeout
+bool AprilTags2Finder::waitForImage()
+{
+  // Initial wait cycle so that camera is definitely up to date.
+  ros::Duration(1/10.0).sleep();
+
+  waiting_ = true;
+  int count = 250;
+  while (--count)
+  {
+    if (!waiting_)
+    {
+      // success
+      return true;
+    }
+    ros::Duration(0.01).sleep();
+    ros::spinOnce();
+  }
+  ROS_ERROR("Failed to get image");
+  return !waiting_;
+}
+
+
+bool AprilTags2Finder::find(robot_calibration_msgs::CalibrationData * msg)
+{
+  geometry_msgs::PointStamped rgbd;
+  geometry_msgs::PointStamped world;
+
+ //  Get image
+ if (!waitForImage())
+  {
+    ROS_ERROR("No image data");
+    return false;
+  }
+
+
+  cv_bridge::CvImagePtr cv_image_ptr;
+  try
+  {
+    cv_image_ptr = cv_bridge::toCvCopy(image_, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return false;
+  }
+
+  boost::shared_ptr<sensor_msgs::CameraInfo> camera_info_ptr(new sensor_msgs::CameraInfo);
+  *camera_info_ptr = depth_camera_manager_.getDepthCameraInfo().camera_info;
+  apriltags2_ros::AprilTagDetectionArray tag_detections =  tag_detector_->detectTags(cv_image_ptr, camera_info_ptr);
+
+  bool found = false;
+
+  apriltags2_ros::AprilTagDetection tag;
+  for (size_t i=0; i<tag_detections.detections.size(); i++)
+  {
+    if (tag_detections.detections[i].id[0] == tag_id_)
+    {
+      tag = tag_detections.detections[i];
+      found = true;
+      break;
+    }
+  }
+
+  // Find apriltag
+  if (found)
+  {
+    ROS_INFO("Found the AprilTag");
+
+    // We have just a point in observation, so use corners and origin of AprilTag
+    std::vector<tf::Vector3> points;
+    points.push_back(tf::Vector3(0,0,0));
+    points.push_back(tf::Vector3(tag_size_/2*1, tag_size_/2*1, 0));
+    points.push_back(tf::Vector3(tag_size_/2*-1, tag_size_/2*1, 0));
+    points.push_back(tf::Vector3(tag_size_/2*1, tag_size_/2*-1, 0));
+    points.push_back(tf::Vector3(tag_size_/2*-1, tag_size_/2*-1, 0));
+    
+    // Create PointCloud2 to publish
+    sensor_msgs::PointCloud2 cloud;
+    cloud.width = 0;
+    cloud.height = 0;
+    cloud.header.stamp = ros::Time::now();
+    cloud.header.frame_id = image_.header.frame_id;
+    sensor_msgs::PointCloud2Modifier cloud_mod(cloud);
+    cloud_mod.setPointCloud2FieldsByString(1, "xyz");
+    cloud_mod.resize(points.size());
+    sensor_msgs::PointCloud2Iterator<float> iter_cloud(cloud, "x");
+
+    // Set msg size
+    int idx_cam = msg->observations.size() + 0;
+    int idx_chain = msg->observations.size() + 1;
+    msg->observations.resize(msg->observations.size() + 2);
+    msg->observations[idx_cam].sensor_name = camera_sensor_name_;
+    msg->observations[idx_chain].sensor_name = chain_sensor_name_;
+         
+    msg->observations[idx_cam].features.resize(points.size());
+    msg->observations[idx_chain].features.resize(points.size());
+
+    // Fill in the headers
+    rgbd.header = image_.header;
+
+    // Fill in message
+    for (size_t i = 0; i < points.size(); ++i)
+    {
+      std::stringstream out;
+      out << "tag_" << tag.id[0];
+      world.header.frame_id = out.str();
+
+      // Point in relation to the chain
+      // Origin
+      world.point.x = points[i].getX();
+      world.point.y = points[i].getY();
+
+      // Point in relation to the camera
+      // Get 3d point
+      tf::Quaternion q;
+      tf::quaternionMsgToTF(tag.pose.pose.pose.orientation, q);
+      tf::Vector3 point = tf::quatRotate(q, points[i]);
+
+      rgbd.point.x = point.getX() + tag.pose.pose.pose.position.x;
+      rgbd.point.y = point.getY() + tag.pose.pose.pose.position.y;
+      rgbd.point.z = point.getZ() + tag.pose.pose.pose.position.z;
+
+      msg->observations[idx_cam].features[i] = rgbd;
+      msg->observations[idx_cam].ext_camera_info = depth_camera_manager_.getDepthCameraInfo();
+      msg->observations[idx_chain].features[i] = world;
+
+      // Visualize
+      iter_cloud[0] = rgbd.point.x;
+      iter_cloud[1] = rgbd.point.y;
+      iter_cloud[2] = rgbd.point.z;
+      ++iter_cloud;
+    }
+
+    // Publish results
+    publisher_.publish(cloud);
+
+    // Found all points
+    return true;
+  }
+
+  return false;
+}
+
+}  // namespace robot_calibration
